8b6f95db5da120bd731c3ea66cd3a8c7f03765bb
Andrew DeVries <andrew@andrewdevries.net>
2026-03-18 17:33:05 -0500

    Add selcal module with frequency table

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod beeps;
 mod ctrl_chars;
 mod cw;
 mod dtmf;
+mod selcal;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/selcal.rs
@@ -0,0 +1,17 @@
+// Designation  Freq    Code
+// Red A        312.6   00
+// Red B        346.7   01
+// Red C        384.6   02
+// Red D        426.6   03
+// Red E        473.2   04
+// Red F        524.8   05
+// Red G        582.1   06
+// Red H        645.7   07
+// Red J        716.1   08
+// Red K        794.3   09
+// Red L        881     10
+// Red M        977.2   11
+// Red P        1083.9  12
+// Red Q        1202.3  13
+// Red R        1333.5  14
+// Red S        1479.1  15
